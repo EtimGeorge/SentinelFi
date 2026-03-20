0a1ec06756255b5dc736967eb38d55eed900a623
{EtimGeorge} <saencrystal@gmail.com>
2026-03-20 04:21:54 +0100

    feat: Add backend TypeScript configuration and documentation for npm run commands.

--- shared/types/role.enum.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum Role {
-    Admin = "Admin",
-    ITHead = "IT Head",
-    Finance = "Finance",
-    OperationalHead = "Operational Head",
-    CEO = "CEO",
-    AssignedProjectUser = "Assigned Project User"
-}
